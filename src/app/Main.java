1dd74171f8eaaa0a20e4c470c5217e4ca8faa596
LukasBager <lukasemilbager@protonmail.com>
2026-03-23 12:44:50 +0100

    Main completed

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,13 @@
 package app;
 
+import ui.MariosPizzabarUI;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        MariosPizzabarUI ui = new MariosPizzabarUI();
+        ui.start();
 
     }
 
